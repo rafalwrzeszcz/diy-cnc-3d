3d570cef55f3704040862926e306edf0e324143d
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2024-04-20 18:45:46 +0200

    Adopted to XTool M1 that I have.
    Tested with xtm1 toolkit (https://github.com/fritzw/xtm1_toolkit).
    
    Shape is flipped and need to set laser power/speed on moves instead of general device control, but it can serve as a good initial GCode generator to work on!

--- gcode.py
+++ gcode.py
@@ -10,22 +10,22 @@ def side_a(start_x, start_y):
     y0 = start_y + margin
 
     return f"""
-G00 X{x0} Y{y0}
-G01 X{x0} Y{y0} Z-{z}
-G01 X{x0 + x} Y{y0}
-G01 X{x0 + x} Y{y0 + y}
-G01 X{x0 + x - size} Y{y0 + y}
-G01 X{x0 + x - size} Y{y0 + y - notch}
-G01 X{x0 + x - size - z} Y{y0 + y - notch}
-G01 X{x0 + x - size - z} Y{y0 + y}
-G01 X{x0 + size + z} Y{y0 + y}
-G01 X{x0 + size + z} Y{y0 + y - notch}
-G01 X{x0 + size} Y{y0 + y - notch}
-G01 X{x0 + size} Y{y0 + y}
-G01 X{x0} Y{y0 + y}
-G01 X{x0} Y{y0}
-G00 X{x0} Y{y0}
-G00 X{x0} Y{y0} Z0
+G0 X{x0} Y{y0}
+G1 X{x0} Y{y0} Z-{z}
+G1 X{x0 + x} Y{y0}
+G1 X{x0 + x} Y{y0 + y}
+G1 X{x0 + x - size} Y{y0 + y}
+G1 X{x0 + x - size} Y{y0 + y - notch}
+G1 X{x0 + x - size - z} Y{y0 + y - notch}
+G1 X{x0 + x - size - z} Y{y0 + y}
+G1 X{x0 + size + z} Y{y0 + y}
+G1 X{x0 + size + z} Y{y0 + y - notch}
+G1 X{x0 + size} Y{y0 + y - notch}
+G1 X{x0 + size} Y{y0 + y}
+G1 X{x0} Y{y0 + y}
+G1 X{x0} Y{y0}
+G0 X{x0} Y{y0}
+G0 X{x0} Y{y0} Z0
 """
 
 def side_b(start_x, start_y):
@@ -33,33 +33,34 @@ def side_b(start_x, start_y):
     y0 = start_y + margin
 
     return f"""
-G00 X{x0} Y{y0}
-G01 X{x0} Y{y0} Z-{z}
-G01 X{x0 + x} Y{y0}
-G01 X{x0 + x} Y{y0 + y - notch}
-G01 X{x0 + x - z} Y{y0 + y - notch}
-G01 X{x0 + x - z} Y{y0 + y}
-G01 X{x0 + z} Y{y0 + y}
-G01 X{x0 + z} Y{y0 + y - notch}
-G01 X{x0} Y{y0 + y - notch}
-G01 X{x0} Y{y0}
-G00 X{x0} Y{y0} Z0
+G0 X{x0} Y{y0}
+G1 X{x0} Y{y0} Z-{z}
+G1 X{x0 + x} Y{y0}
+G1 X{x0 + x} Y{y0 + y - notch}
+G1 X{x0 + x - z} Y{y0 + y - notch}
+G1 X{x0 + x - z} Y{y0 + y}
+G1 X{x0 + z} Y{y0 + y}
+G1 X{x0 + z} Y{y0 + y - notch}
+G1 X{x0} Y{y0 + y - notch}
+G1 X{x0} Y{y0}
+G0 X{x0} Y{y0} Z0
 """
 
-print(f"""%
-G00
-G17
+# we also used to send:
+# G17, G54 at the beginning
+# G28 X0 Y0, M30 at the end
+# but xtm1 seems to not handle it
+
+print(f"""
+G0
 G21
-G54
 G90
-M04
-M08
+M8
+M4
 {side_a(0, 0)}
 {side_a(0, y + margin)}
 {side_b(0, 2 * (y + margin))}
 {side_b(0, 3 * (y + margin))}
-M05
-M09
-G28 X0 Y0
-M30
-%""")
+M5
+M9
+""")
